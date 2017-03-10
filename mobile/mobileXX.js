9fda2588dc81850b2e54037c1026549a1ce08581
Bill Haslag <bhaslag@gmail.com>
2017-03-10 12:51:53 +0100

    Add files via upload

---
+++ mobile/mobileXX.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('#imgBox2').click(function(){
+    if ($('#imgBox2').hasClass('slide-down2')) {
+      $('#imgBox2').addClass('slide-up2');
+      $('#imgBox2').removeClass('slide-down2');
+    }
+    else {
+      $('#imgBox2').removeClass('slide-up2');
+      $('#imgBox2').addClass('slide-down2');
+    }
+  });
+
+
+  
+  $('#imgBox1').click(function(){
+    if ($('#imgBox1').hasClass('slide-up1')) {
+      $('#imgBox1').addClass('slide-down1');
+      $('#imgBox1').removeClass('slide-up1');
+    }
+    else {
+      $('#imgBox1').removeClass('slide-down1');
+      $('#imgBox1').addClass('slide-up1');
+    }
+  });
+});
